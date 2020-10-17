c60f770bd070e096c1e815ac58be1a172f6a4883
Avinash Koshal <31934725+aaavinash85@users.noreply.github.com>
2020-10-17 19:10:33 +0530

    Update color_detection.py

--- color_detection.py
+++ color_detection.py
@@ -2,9 +2,6 @@ import cv2
 import numpy as np
 import pandas as pd
 
-
-
-
 img = cv2.imread('images/col.jpg')
 
 clicked = False
@@ -40,24 +37,16 @@ while(1):
 
     cv2.imshow("image",img)
     if (clicked):
-   
-       
         cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
-
-        
         text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
-        
-       
         cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
-
-        
         if(r+g+b>=600):
             cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
             
         clicked=False
 
    
-    if cv2.waitKey(20) & 0xFF ==27:
+    if cv2.waitKey(33) & 0xFF ==27:
         break
     
 cv2.destroyAllWindows()
